245080647e662c6f4e52a42864242c9f79eb442b
Katbiz <ramothamok77@gamil.com>
2024-08-22 22:06:43 +0200

    index updates

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 app.use(express.json());
